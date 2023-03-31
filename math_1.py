b83720536191c63432b23d50187dd03e889c513e
Your Name <kilasaniso@gmail.com>
2023-03-31 21:04:34 +0300

    first commit

---
+++ math_1.py
@@ -0,0 +1,20 @@
+def add(a,b):
+    sum=a+b
+    return sum
+
+# divide
+def divide(m,w):
+    result=m / w
+    return result
+# subtraction
+def subtraction(x,y):
+    answer=x-y
+    return answer
+#Mulitplication
+def multiply(t,r):
+    product=t*r
+    return product
+#remainder
+def remainder(b,i):
+    remainders=b%i
+    return remainder
